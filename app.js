5800f1f3f9ff8ddcc571e41fd673bd2e0141556a
RoxanaCS <roxale.cares@gmail.com>
2018-02-16 10:21:02 -0300

    primera pregunta

--- app.js
+++ app.js
@@ -1,15 +1,13 @@
 fetch('https://opentdb.com/api.php?amount=15&category=20&type=multiple')
   .then(function(response) {
-    //Turns the the JSON into a JS object
     return response.json();
   })
   .then(function(data) {
     console.log(data);
-    $('#start').on('click', function()  {
-      data.results.forEach(function(element)  {
-        console.log(element.question);
+    $('#start').on('click', function() {
+      console.log(data.results[0].question);
       });
-    });
-  })
-  .catch(function(error){
+
+    })
+  .catch(function(error) {
   });
