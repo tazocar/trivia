9731712e9e6197afb9dc35b469cfd89e983b5f3b
RoxanaCS <roxale.cares@gmail.com>
2018-02-18 10:00:32 -0300

    inicio de sesión con firebase

--- src/js/app.js
+++ src/js/app.js
@@ -5,29 +5,92 @@ console.log(data.results[0].incorrect_answers[2]);
 console.log(data.results[0].correct_answer);
 console.log(data.results[0].difficulty);
 console.log(data.results[0].category);*/
+// inicio sesión con firebase
+$(document).ready(function() {
+  // para que siempre esté deslogeado
+  firebase.auth().signOut();
+  $('#start').hide();
+  $('button').hide();
+});
+// Al hacer click en el boton de registro con google:
+document.getElementById('btnsignUp').addEventListener('click', GoogleSignUp, false);
+// Initialize Firebase
+var config = {
+  apiKey: 'AIzaSyCoCBlZhycLbNacfdCEbzWcHcIOUT05cBw',
+  authDomain: 'trivia-54132.firebaseapp.com',
+  databaseURL: 'https://trivia-54132.firebaseio.com',
+  projectId: 'trivia-54132',
+  storageBucket: '',
+  messagingSenderId: '920398654997'
+};
+firebase.initializeApp(config);
+// función de ingreso con google
+var token = 'none';
+var user = 'none';
+var email = 'none';
+// guardar los usuarios que se registren
+var userData = firebase.database().ref('users');
+function GoogleSignUp() {
+  if (!firebase.auth().currentUser) {
+    // para saber si el usuario se ha conectado
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      token = result.credential.accessToken;
+      user = result.user;
+		  email = user.email;
+      $('#btnsignUp').hide() && $('.choices').show();
+      // guardar el nombre de usuario en firebase
+      userData.orderByChild('email').equalTo(user.email).on('value', function(snapshot) {
+        // console.log(snapshot.val());
+        if (snapshot.val() === null) {
+          // console.log('Nuevo Usuario');
+          userData.push({
+            photo: user.photoURL,
+            name: user.displayName,
+            email: user.email
+          });
+        } else {
+          // console.log('Usuario Existente');
+        }
+      });
+    }).catch(function(error) {
+      var errorCode = error.code;
+      var errorMessage = error.message;
+      var errorEmail = error.email;
+      var credencial = error.credencial;
+      // console.log(errorCode);
+      if (errorCode === 'auth/account-exists-with-different-credential') {
+        alert('Es el mismo usuario');
+      }
+    });
+  } else {
+    firebase.auth().signOut();
+  }
+}
 
 let anime = 'https://opentdb.com/api.php?amount=15&category=31&type=multiple';
 let cience = 'https://opentdb.com/api.php?amount=15&category=20&type=multiple';
 let animals = 'https://opentdb.com/api.php?amount=10&encode=base64';
 $('.trueFalse').hide();
 $('.multiple').hide();
-$('#start').hide(); 
+$('#start').hide();
 
 $('.choices').on('click', function() {
   $('.choices').hide();
   $('.trueFalse').show();
-  $('.multiple').show(); 
+  $('.multiple').show();
   optionsToPlay($(this).attr('data-api'));
 });
 
 function optionsToPlay(result) {
   $('.trueFalse').on('click', function() {
     displayQuestions(result + '&type=boolean');
-    $('#start').show(); 
+    $('#start').show();
   });
   $('.multiple').on('click', function() {
     displayQuestions(result + '&type=multiple');
-    $('#start').show(); 
+    $('#start').show();
   });
 }
 function displayQuestions(typeChoice) {
@@ -49,9 +112,9 @@ function displayQuestions(typeChoice) {
         $('.result').html('');
         // poner las alternativas en un arreglo
         var alternatives = [];
-        data.results[counter].incorrect_answers.forEach(function(element){
+        data.results[counter].incorrect_answers.forEach(function(element) {
           alternatives.push(element);
-        })
+        });
         // alternatives.push(data.results[counter].incorrect_answers[0]);
         // alternatives.push(data.results[counter].incorrect_answers[1]);
         // alternatives.push(data.results[counter].incorrect_answers[2]);
